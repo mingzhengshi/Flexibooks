752f60f37b820033a9f33bb779e00f34ca4931af
Mingzheng Shi <mingzhengshi@gmail.com>
2014-09-29 17:05:29 +1000

    visual diff

--- wp-content/plugins/visualdiff/visualdiff.php
+++ wp-content/plugins/visualdiff/visualdiff.php
@@ -8,6 +8,9 @@ Description: Visual Display of Revision Differences
 <?php  
 add_filter( 'the_content', 'add_content' );
 add_action( 'admin_footer', 'visualdiff_admin_footer' );
+//add_action( 'post_submitbox_misc_actions', 'add_revision_diff_button' );
+
+add_action( 'add_meta_boxes', 'add_meta_box_revision' );
 
 function add_content( $content ) {
     $x = 1;
@@ -17,9 +20,36 @@ function add_content( $content ) {
 }
 
 function visualdiff_admin_footer() {
-		// Only load js on revision screen
-		if (get_current_screen()->id == 'revision') {
-			wp_enqueue_script( 'visualdiff', plugins_url( 'js/visualdiff.js' , __FILE__ ), array( 'jquery' ));
-        }
+    // Only load js on revision screen
+    if (get_current_screen()->id == 'post') {
+	    wp_enqueue_script( 'visualdiff', plugins_url( 'js/visualdiff.js' , __FILE__ ), array( 'jquery' ));
     }
+}
+
+/*
+function add_revision_diff_button()
+{
+?>
+    <div class="misc-pub-section my-options" align="right">
+        <input type="submit" value="Compare Revisions" class="button-secondary" id="button-compare-revisions-test">
+    </div>
+<?php
+}
+*/
+
+function add_meta_box_revision() {
+    global $post;
+    if ($post) {
+        add_meta_box('meta_box_post_revision', 'Revision', 'meta_box_post_revision_callback', null, 'side', 'core' );
+    }
+}
+
+function  meta_box_post_revision_callback() {
+?>
+    <div align="right">
+		<input id="button-compare-revisions" type="button" class="button" value="Compare Revisions" style="margin-top: 5px;">
+    </div>
+<?php    
+}
+
 ?>  
