8c3ad1cccc6594637265290e9a0d821f0d54c883
Mingzheng Shi <mingzhengshi@gmail.com>
2014-09-29 15:31:45 +1000

    visual diff

---
+++ wp-content/plugins/visualdiff/js/visualdiff.js
@@ -0,0 +1,28 @@
+(function ($) {
+    console.log("visualdiff.js...");
+
+    $(document).ready(function () {
+        var revisions = window.wp.revisions,
+        SELF = this;
+
+        if (typeof revisions !== 'undefined') {
+            console.log(revisions);
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}(jQuery));
--- wp-content/plugins/visualdiff/visualdiff.php
+++ wp-content/plugins/visualdiff/visualdiff.php
@@ -1,17 +1,25 @@
 <?php  
 /* 
 Plugin Name: Visual Diff 
-Description: Visual Display of Revision Comparisons
+Description: Visual Display of Revision Differences
 */  
 ?>  
 
 <?php  
 add_filter( 'the_content', 'add_content' );
- 
+add_action( 'admin_footer', 'visualdiff_admin_footer' );
+
 function add_content( $content ) {
     $x = 1;
     $y = 2;
     $z = $x + $y;
     return $content . '<p>Thanks for Reading! </p>' . $z;
 }
+
+function visualdiff_admin_footer() {
+		// Only load js on revision screen
+		if (get_current_screen()->id == 'revision') {
+			wp_enqueue_script( 'visualdiff', plugins_url( 'js/visualdiff.js' , __FILE__ ), array( 'jquery' ));
+        }
+    }
 ?>  
