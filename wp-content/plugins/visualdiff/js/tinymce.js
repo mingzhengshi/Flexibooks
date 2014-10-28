6873c90d8a8e85a84d79ff52d58e61ccbb446d7a
Mingzheng Shi <mingzhengshi@gmail.com>
2014-10-28 15:42:20 +1100

    tinymce plugin

--- wp-content/plugins/visualdiff/js/tinymce.js
+++ wp-content/plugins/visualdiff/js/tinymce.js
@@ -1,16 +1,23 @@
 jQuery(document).ready(function ($) {
-    tinymce.PluginManager.add('fb_folding_editor', function (editor, url) {        
+    tinymce.PluginManager.add('fb_folding_editor', function (editor, url) {
+
+        // events
         editor.on('init', function () {
             var left_column = document.createElement('div');
             left_column.style.position = 'absolute';
             left_column.style.top = 0;
             left_column.style.left = 0;
-            left_column.style.width = '15px';
+            left_column.style.width = '12px';
             left_column.style.height = '100%';
             left_column.style.backgroundColor = '#e8e8e8';
             left_column.id = 'fb_tinymce_left_column';
             editor.getBody().appendChild(left_column);
 
+            setupIcons();
+        });
+
+        editor.on('change', function (e) {
+            console.log('change event', e);
             setupIcons();
         });
 
@@ -20,26 +27,65 @@ jQuery(document).ready(function ($) {
         });
         */
 
-        function setupIcons() {
-            /*
-            $("h1").each(function (index) {
-                console.log(index + ": " + $(this).text());
-                var icon_id = 'icon-' + $(this).id;
-                var position = $(this).position;
-            });
-            */
-            
-            var h1s = editor.getBody().getElementsByTagName('h1');
-            if (h1s != null && h1s.length > 0) {
-                for (var i = 0; i < h1s.length; i++) {
-                    var icon_id = 'icon-' + h1s[i].id;
-                    var rect = h1s[i].getBoundingClientRect();
 
-                    var jid = '#' + h1s[i].id;
-                    var pos = $(jid).position();
+
+        // functions
+        function setupIcons() {
+            $(editor.getBody()).find('.fb_tinymce_left_column_icon').remove(); // clear all exisint icons
+
+            $(editor.getBody()).find('h1').each(function (index) {
+                console.log(index + ": " + $(this).text());
+                var iconID = 'icon-' + $(this).attr('id');
+                var offset = $(this).offset(); // absolute position relative to the document
+                var height = $(this).height();
+                createIcon(iconID, offset.top);
+            });
+
+            $(editor.getBody()).find('h2').each(function (index) {
+                console.log(index + ": " + $(this).text());
+                var iconID = 'icon-' + $(this).attr('id');
+                var offset = $(this).offset(); // absolute position relative to the document
+                var height = $(this).height();
+                createIcon(iconID, offset.top);
+            });
+
+            $(editor.getBody()).find('h3').each(function (index) {
+                console.log(index + ": " + $(this).text());
+                var iconID = 'icon-' + $(this).attr('id');
+                var offset = $(this).offset(); // absolute position relative to the document
+                var height = $(this).height();
+                createIcon(iconID, offset.top);
+            });
+
+            setupIconEvents();
+        }
+
+        function setupIconEvents() {
+            $(editor.getBody()).find('.fb_tinymce_left_column_icon').hover(
+                // handlerIn
+                function () {
+                    $(this).css('cursor', 'pointer');
+                },
+                // handlerOut
+                function () {
+                    $(this).css('cursor', 'text');
                 }
-            }
-            
+            );
+        }
+
+        function createIcon(id, top) {
+            var icon = document.createElement('div');
+            icon.className = 'fb_tinymce_left_column_icon';
+            icon.id = id;
+            icon.innerHTML = '&#8863'; // minus with box; '&#8862' is plus with box
+            icon.style.position = 'absolute';
+            icon.style.top = top + 'px';
+            icon.style.left = '-0.5px';
+            //icon.style.width = '8px';
+            //icon.style.height = '8px';
+            //icon.style.backgroundColor = '#ff0000';
+
+            editor.getBody().appendChild(icon);
         }
 
     });
