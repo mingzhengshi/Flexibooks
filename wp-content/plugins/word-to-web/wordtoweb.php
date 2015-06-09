8baa7254de0721d5154a04c2041a20998cb7bad0
Mingzheng Shi <mingzhengshi@gmail.com>
2015-06-09 16:31:52 +1000

    parse html and css files

--- wp-content/plugins/word-to-web/wordtoweb.php
+++ wp-content/plugins/word-to-web/wordtoweb.php
@@ -6,27 +6,58 @@ Version: 1.0
 Author: Mingzheng Shi
 */  
 
-//$autoloader = plugins_url( "vendor/autoload.php", __FILE__ );
 require_once( "vendor/autoload.php" );
+//require_once( 'simple_html_dom.php' );
 
 add_action( 'admin_head', 'ww_admin_head' );
 
-function ww_admin_head() {   
-    $file = plugins_url( 'derived.css' , __FILE__ );
-    //$content = file($file);
-    $content = "html .mceContentBody {
-	            max-width: 800px;
-                }
 
-                h1.main-heading-1 {
-                    padding-left: 35px;
-                    background-color: #d1e7f7;
-                    color: #1f6ca5;
-                    font-size: 35pt;
-                }";
-    $css_parser = new Sabberworm\CSS\Parser($content);
-    $css_doc = $css_parser->parse();
+
+function ww_admin_head() {   
+    //$filename = plugins_url( 'derived.txt' , __FILE__ );
+    $ww_file_path = 'C:/Users/Mingzheng/Documents/GitHub/Macmillan/Files/html/';
+    
+    $filename = $ww_file_path . '34_Risk-taking_combo.htm';
+    $file = fopen($filename, "r");
+    if (!$file) return;
+    $content = fread($file,filesize($filename));
+    fclose($file);
+    
+    $html = str_get_html($content);  
+    
+    //----------------------------------------------------------
+    // css
+    
+    $style = null;
+    foreach($html->find('style') as $element) {
+        $style = $element->innertext;        
+        break;
+    }
+    if (!$style) return;
+    $style = trim($style);
+    
+    // clean beginning and end of a string
+    $prefix = '<!--';
+    if (substr($style, 0, strlen($prefix)) == $prefix) {
+        $style = substr($style, strlen($prefix));
+    } 
+    $appendix = '-->';
+    if (substr($style, strlen($style) - strlen($appendix), strlen($style)) == $appendix) {
+        $style = substr($style, 0, strlen($style) - strlen($appendix));
+    } 
+    $style = trim($style);
+    
+    $css_parser = new Sabberworm\CSS\Parser($style);
+    $css = $css_parser->parse();
+    ww_check_css($css); 
 }
 
+function ww_check_css() {
+    // 1. remove all styles started with 'mso' if they are not used in the html
+    
+    // 2. for all other styles
+    // if it is not used in html, then remove and skip
+    // if it is used in html, check if it already exists in the current css list; if yes, rename it if necessary
+}
 
 ?>
